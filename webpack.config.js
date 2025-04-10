41bde8f474a7763de9669c4c15a6dd58ebfe5749
enod <enod2008@gmail.com>
2025-04-10 14:04:15 +1000

    2nd

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -36,7 +37,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/data', to: 'data' }
+      ],
+    }) 
   ],
   devServer: {
     historyApiFallback: true,
